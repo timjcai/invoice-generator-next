6d52e283d62b609b3b515943425379328aa08b0a
timjcai <tim.j.cai@gmail.com>
2023-12-10 21:48:34 +1100

    feat: displaying userData
    what?
    > created component to handle Location data - able to style this going forward to adjust how we want to display Location
    > need to restyle and format how we want to present information

--- src/app/components/InvoiceTemplate/InvoicePreview.tsx
+++ src/app/components/InvoiceTemplate/InvoicePreview.tsx
@@ -1,6 +1,8 @@
+"use client";
+
 import React, { FC } from "react";
 import { BubbleWrapper } from ".";
-import { BuyerType, SellerType } from "@/app/types";
+import { BuyerType, LocationType, SellerType } from "@/app/types";
 
 export type InvoiceType = {
     invoiceNumber: number;
@@ -18,12 +20,47 @@ export const InvoicePreview: FC<InvoiceType> = ({
             <section>
                 <h1>Invoice #{invoiceNumber}</h1>
             </section>
-            <section>
-                <h1>Title</h1>
+            <section className="flex flex-row gap-8">
+                <div id="sellerDetails">
+                    <div>Business Name: {sellerDetails.businessName}</div>
+                    <div>ABN: {sellerDetails.ABN}</div>
+                    <div>
+                        Location:
+                        <LocationGrid {...sellerDetails.businessLocation} />
+                    </div>
+                </div>
+                <div id="buyerDetails">
+                    <div>Business Name: {buyerDetails.businessName}</div>
+                    <div>ABN: {buyerDetails.ABN}</div>
+                    <div>
+                        Location:
+                        <LocationGrid {...buyerDetails.businessLocation} />
+                    </div>
+                </div>
             </section>
             <section>
                 <h1>Title</h1>
             </section>
         </BubbleWrapper>
+    );
+};
+
+const LocationGrid: FC<LocationType> = ({
+    streetLine1,
+    streetLine2,
+    country,
+    suburb,
+    state,
+    postcode,
+}) => {
+    return (
+        <div>
+            <p>{streetLine1},</p>
+            {streetLine2 && <p>{streetLine2},</p>}
+            <p>
+                {suburb} {state} {postcode}
+            </p>
+            <p>{country}</p>
+        </div>
     );
 };
