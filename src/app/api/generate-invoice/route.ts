e66bfe8d00ff49ca3601d4a48f00f25df6a80e9e
timjcai <tim.j.cai@gmail.com>
2024-01-21 22:15:34 +1100

    chores: removing unnecessary api endpoint

--- src/app/api/generate-invoice/route.ts
+++
@@ -1,6 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const data = req.body
-    return res.status(200).json({message: 'postrequest complete'});
-}
