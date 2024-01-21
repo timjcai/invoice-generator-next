cef42901b02c3cce31a42cf18906cd317816daae
timjcai <tim.j.cai@gmail.com>
2024-01-21 22:13:17 +1100

    chores: issues with building app for Netlify - deployment

--- src/app/api/generate-invoice/route.ts
+++ src/app/api/generate-invoice/route.ts
@@ -2,6 +2,5 @@ import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const data = req.body
-    await generateInvoice(data);
     return res.status(200).json({message: 'postrequest complete'});
 }
