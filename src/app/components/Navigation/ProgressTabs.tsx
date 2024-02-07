1f5ce1557e18bd62b93215285b0e26d1377ac0e2
timjcai <tim.j.cai@gmail.com>
2024-02-08 00:17:18 +1100

    feat: progress bars
    - separated using CSS grid styling
    - as step progresses - they change color

--- src/app/components/Navigation/ProgressTabs.tsx
+++ src/app/components/Navigation/ProgressTabs.tsx
@@ -14,21 +14,44 @@ export const ProgressTabs = () => {
     } = useMultistepForm([<p>1</p>, <p>2</p>, <p>3</p>, <p>4</p>, <p>5</p>]);
 
     return (
-        <div className="flex flex-row">
-            {!isFirstStep && (
-                <button onClick={back}>
-                    <Icon label="back" />
-                </button>
-            )}
-            <div>
-                <p>step {currentStepIndex}</p>
-                <p>{step}</p>
+        <div className="flex flex-col bg-blue-500 w-full py-1 px-2">
+            <div className="flex flex-row justify-between">
+                {!isFirstStep ? (
+                    <button onClick={back}>
+                        <Icon label="back" />
+                    </button>
+                ) : (
+                    <button></button>
+                )}
+                <div>
+                    <p>step {currentStepIndex}</p>
+                    <p>{step}</p>
+                </div>
+                {!isLastStep ? (
+                    <button onClick={next}>
+                        <Icon label="next" />
+                    </button>
+                ) : (
+                    <button></button>
+                )}
             </div>
-            {!isLastStep && (
-                <button onClick={next}>
-                    <Icon label="next" />
-                </button>
-            )}
+            <div className={`grid grid-cols-5 gap-4 h-[12px]`}>
+                {steps.map((item, index) => (
+                    <div>
+                        {currentStepIndex > index ? (
+                            <div
+                                className="bg-black h-2 col-span-1 rounded-lg"
+                                key={index}
+                            ></div>
+                        ) : (
+                            <div
+                                className="bg-white h-2 col-span-1 rounded-lg"
+                                key={index}
+                            ></div>
+                        )}
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
